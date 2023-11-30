65e131526a9a9b86e864eb98a88b42359ade90e9
Stanimir Dimitrov <evlution63@gmail.com>
2023-11-30 17:29:06 +0200

    style: onHover styles

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -19,12 +19,19 @@ const sections: Section[] = [
 export const Navbar: React.FunctionComponent = () => {
   return (
     <section id='menu' className='border-b'>
-      <nav className='flex justify-between py-4 max-w-5xl m-auto'>
+      <nav className='flex justify-between items-center py-4 max-w-5xl m-auto'>
         <span className='font-permanentMarker text-lg'>Stanimir Dimitrov</span>
 
         <ul className='flex gap-12 items-center'>
           {sections.map((s) => (
-            <li className='list-none'>{s.name}</li>
+            <li className='list-none'>
+              <a
+                href='#'
+                className='hover:text-[#6E57E0] hover:border-b cursor-pointer py-1'
+              >
+                <span>{s.name}</span>
+              </a>
+            </li>
           ))}
         </ul>
       </nav>
