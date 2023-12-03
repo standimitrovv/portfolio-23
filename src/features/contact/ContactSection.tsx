2e2adc78ebd095a5685f43f402ef30453099935c
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-03 17:12:04 +0200

    refactor: create a contactMessage component

--- src/features/contact/ContactSection.tsx
+++ src/features/contact/ContactSection.tsx
@@ -1,4 +1,5 @@
 import { SocialMediaLinks } from '../../components/SocialMediaLinks';
+import { ContactMessage } from './components/ContactMessage';
 
 export const ContactSection = () => {
   return (
@@ -9,20 +10,7 @@ export const ContactSection = () => {
 
       <div className='flex justify-between'>
         <div className='flex flex-col'>
-          <div className='mb-8'>
-            <span>
-              Feel free to reach out if you are in need of a Frontend Developer.
-            </span>
-            <br />
-            <span>
-              Whether that means you need a website, web app, or you are looking
-              to fill a full-time position.
-            </span>
-            <br />
-            <span>
-              I am <strong>always</strong> open to a conversation.
-            </span>
-          </div>
+          <ContactMessage />
           <SocialMediaLinks />
         </div>
         <div>contact form</div>
---
+++ src/features/contact/components/ContactMessage.tsx
@@ -0,0 +1,18 @@
+export const ContactMessage = () => {
+  return (
+    <div className='mb-8'>
+      <span>
+        Feel free to reach out if you are in need of a Frontend Developer.
+      </span>
+      <br />
+      <span>
+        Whether that means you need a website, web app, or you are looking to
+        fill a full-time position.
+      </span>
+      <br />
+      <span>
+        I am <strong>always</strong> open to a conversation.
+      </span>
+    </div>
+  );
+};
