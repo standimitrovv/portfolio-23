711fab818086aea643b7e330c59c059e8734b3ad
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-04 18:09:18 +0200

    feat: add the ToastContainer to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { ContactSection } from './features/contact/ContactSection';
 import { Navbar } from './features/navbar/Navbar';
 import { MobileMenuProvider } from './features/navbar/state/MobileMenuProvider';
+import { useNotifications } from './hooks/UseNotifications';
 
 export const App = () => {
+  const { containerConfiguration } = useNotifications();
+
   return (
     <main className='font-geistMono text-sm leading-7'>
       <MobileMenuProvider>
@@ -11,6 +16,8 @@ export const App = () => {
         <footer className='bg-[#fafafa]'>
           <ContactSection />
         </footer>
+
+        <ToastContainer {...containerConfiguration} />
       </MobileMenuProvider>
     </main>
   );
