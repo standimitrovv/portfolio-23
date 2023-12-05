a835eb3a02f586f2ebfaf16920104554485c8b23
Stanimir Dimitrov <evlution63@gmail.com>
2023-12-05 18:03:04 +0200

    wip: form validation

--- src/features/contact/components/ContactForm.tsx
+++ src/features/contact/components/ContactForm.tsx
@@ -1,8 +1,23 @@
-import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
+import {
+  ArrowUpRightIcon,
+  ExclamationTriangleIcon,
+} from '@heroicons/react/24/solid';
 import { useState } from 'react';
 import { LoadingSpinner } from '../../../components/LoadingSpinner';
 import { useNotifications } from '../../../hooks/UseNotifications';
 import { useSendEmail } from '../hooks/UseSendEmail';
+
+interface FormValidationProperties {
+  isNameValid: boolean | undefined;
+  isEmailValid: boolean | undefined;
+  isMessageValid: boolean | undefined;
+}
+
+const INITIAL_FORM_VALUES: FormValidationProperties = {
+  isNameValid: undefined,
+  isEmailValid: undefined,
+  isMessageValid: undefined,
+};
 
 export const ContactForm = () => {
   const { sendEmail } = useSendEmail();
@@ -18,15 +33,82 @@ export const ContactForm = () => {
 
   const [isSubmittingForm, setIsSubmittingForm] = useState<boolean>(false);
 
-  const isSubmitButtonDisabled =
-    !name || !email || !message || isSubmittingForm;
+  const [formValidation, setFormValidation] =
+    useState<FormValidationProperties>(INITIAL_FORM_VALUES);
 
-  const clearFormFields = () => {
+  const isSubmitButtonDisabled =
+    !Object.values(formValidation).every(Boolean) || isSubmittingForm;
+
+  const handleNameInputFieldValidation = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    const fieldValue = e.currentTarget.value;
+
+    if (fieldValue.length < 2) {
+      setFormValidation((prevState) => ({
+        ...prevState,
+        isNameValid: false,
+      }));
+
+      return;
+    }
+
+    setFormValidation((prevState) => ({
+      ...prevState,
+      isNameValid: true,
+    }));
+  };
+
+  const handleEmailFieldValidation = (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    const fieldValue = e.currentTarget.value;
+
+    if (
+      !!fieldValue.match('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$') === false
+    ) {
+      setFormValidation((prevState) => ({
+        ...prevState,
+        isEmailValid: false,
+      }));
+
+      return;
+    }
+
+    setFormValidation((prevState) => ({
+      ...prevState,
+      isEmailValid: true,
+    }));
+  };
+
+  const handleMessageFieldValidation = (
+    e: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    const fieldValue = e.currentTarget.value.split(' ').join('').trim();
+
+    if (fieldValue.length < 8) {
+      setFormValidation((prevState) => ({
+        ...prevState,
+        isMessageValid: false,
+      }));
+
+      return;
+    }
+
+    setFormValidation((prevState) => ({
+      ...prevState,
+      isMessageValid: true,
+    }));
+  };
+
+  const defaultForm = () => {
     setName('');
 
     setEmail('');
 
     setMessage('');
+
+    setFormValidation(INITIAL_FORM_VALUES);
   };
 
   const handleSubmit = async () => {
@@ -41,7 +123,7 @@ export const ContactForm = () => {
     } finally {
       setIsSubmittingForm(false);
 
-      clearFormFields();
+      defaultForm();
     }
   };
 
@@ -50,45 +132,72 @@ export const ContactForm = () => {
       <label htmlFor='name' className='font-semibold'>
         Name
       </label>
-      <input
-        type='text'
-        id='name'
-        placeholder='What is your name?'
-        className='py-2 px-4 outline-none rounded-md mt-2 mb-4'
-        disabled={isSubmittingForm}
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
+      <div className='flex items-center relative mt-2 mb-4'>
+        <input
+          type='text'
+          id='name'
+          placeholder='What is your name?'
+          className={`py-2 pl-4 pr-10 outline-none rounded-md w-full ${
+            formValidation.isNameValid === false ? 'border border-red-500' : ''
+          }`}
+          disabled={isSubmittingForm}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onKeyUp={handleNameInputFieldValidation}
+        />
+        {formValidation.isNameValid === false &&
+          renderIcon('Please enter more than 2 characters!')}
+      </div>
       <label htmlFor='email' className='font-semibold'>
         Email
       </label>
-      <input
-        type='email'
-        id='email'
-        placeholder='What is your email?'
-        className='py-2 px-4 outline-none rounded-md mt-2 mb-4'
-        disabled={isSubmittingForm}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+      <div className='flex items-center relative mt-2 mb-4'>
+        <input
+          type='email'
+          id='email'
+          placeholder='What is your email?'
+          className={`py-2 pl-4 pr-10 outline-none rounded-md w-full ${
+            formValidation.isEmailValid === false ? 'border border-red-500' : ''
+          }`}
+          disabled={isSubmittingForm}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onKeyUp={handleEmailFieldValidation}
+        />
+        {formValidation.isEmailValid === false &&
+          renderIcon('Please enter a valid email!')}
+      </div>
 
       <label htmlFor='message' className='font-semibold'>
         Message
       </label>
-      <textarea
-        id='message'
-        rows={5}
-        cols={12}
-        placeholder='How can I help you?'
-        className='py-2 px-4 outline-none rounded-md mt-2 mb-4'
-        disabled={isSubmittingForm}
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
+      <div className='flex items-center relative mt-2 mb-4 '>
+        <textarea
+          id='message'
+          rows={5}
+          cols={12}
+          placeholder='How can I help you?'
+          className={`py-2 pl-4 pr-10 outline-none rounded-md w-full ${
+            formValidation.isMessageValid === false
+              ? 'border border-red-500'
+              : ''
+          }`}
+          disabled={isSubmittingForm}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyUp={handleMessageFieldValidation}
+        />
+        {formValidation.isMessageValid === false &&
+          renderIcon('Please enter more than 8 characters!', {
+            isLargeField: true,
+          })}
+      </div>
 
       <button
         type='submit'
-        className='bg-activeText py-2 rounded-md text-white flex justify-center items-center gap-2 group cursor-pointer'
+        className={`bg-activeText py-2 rounded-md text-white flex justify-center items-center gap-2 ${
+          isSubmitButtonDisabled ? 'opacity-20' : 'group cursor-pointer'
+        }`}
         disabled={isSubmitButtonDisabled}
         onClick={handleSubmit}
       >
@@ -104,3 +213,23 @@ export const ContactForm = () => {
     </form>
   );
 };
+
+interface Options {
+  isLargeField?: boolean;
+}
+
+function renderIcon(tooltipText: string, options?: Options) {
+  return (
+    <div
+      className={`absolute right-3 group ${options?.isLargeField && 'top-3'}`}
+    >
+      <ExclamationTriangleIcon className='w-6 h-6 fill-red-400' />
+      <div
+        role='tooltip'
+        className='opacity-0 group-hover:opacity-100 min-w-max absolute -top-11 -left-32 px-3 py-2 text-sm text-white bg-gray-900 rounded-lg shadow-sm'
+      >
+        {tooltipText}
+      </div>
+    </div>
+  );
+}
